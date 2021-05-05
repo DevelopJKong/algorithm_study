9c1dd3b6f867b1aac98fbecc4737c5ac59616031
DevelopJKong <61773021+DevelopJKong@users.noreply.github.com>
2021-05-05 16:19:23 +0900

    Add files via upload

---
+++ algorithm_myblog/src/day1/pro01.java
@@ -0,0 +1,18 @@
+package day1;
+
+public class pro01 {
+	public static void main(String[] args) {
+		
+
+	int sum = 0;
+	
+	for (int i=1; i <= 100; i++) {
+		sum = i + sum; 
+		System.out.println(sum);
+	}
+	System.out.println("===============");	
+	System.out.println(sum);
+		
+
+	}
+}
